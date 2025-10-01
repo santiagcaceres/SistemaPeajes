497a1321d05fe898a357186d815960f7345a3b91
santiagcaceres <caceressantiago2001@gmail.com>
2025-09-30 21:54:16 -0300

    Primer commit - proyecto Spring Boot

---
+++ proyectoDda/src/main/java/ProyectoDda/proyectoDda/ProyectoDdaApplication.java
@@ -0,0 +1,13 @@
+package ProyectoDda.proyectoDda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDdaApplication.class, args);
+	}
+
+}
---
+++ proyectoDda/src/test/java/ProyectoDda/proyectoDda/ProyectoDdaApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoDda.proyectoDda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
